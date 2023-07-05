e755f88ac9e8fc0673c218a96a14c312fcea1fdb
Qetzin <qetzin23@gmail.com>
2023-07-05 17:09:03 +0200

    Add project files.

---
+++ DAL/NotesDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class NotesDbContext : DbContext
+    {
+        public DbSet<Note> Notes { get; set; }
+        public NotesDbContext(){
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=NotesDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
+        }
+    }
+}
